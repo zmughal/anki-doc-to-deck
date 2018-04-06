4071df2342ef9985cca17ed117babaccff2925f1
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-04-06 15:49:55 -0400

    Make slightly more release-worthy

--- lib/Anki/DocGen.pm
+++ lib/Anki/DocGen.pm
@@ -1,6 +1,20 @@
 package Anki::DocGen;
+# ABSTRACT: Generate Anki decks for Image Occlusion Enhanced using pages from documents
 
 use strict;
 use warnings;
 
 1;
+
+=head1 SYNOPSIS
+
+  run-pdf-to-deck.pl [PDF or DOCX files...] Deck.apkg
+
+=head1 DESCRIPTION
+
+Generates an Anki deck that can be edited using L<Image Occlusion
+Enhanced|https://github.com/glutanimate/image-occlusion-enhanced> by
+extracting all the pages from the given documents. The fields/tags for each
+card are configurable by using a subclass of C<Anki::DocGen::MetadataGen>.
+
+=cut
